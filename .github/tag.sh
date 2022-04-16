5aa0bc707fa3118185d383f1f106634f77293975
Ashwani Sharma <i14a23h19a@gmail.com>
2022-04-16 14:09:08 +0530

    ci: Add Docker image tagging suport

---
+++ .github/tag.sh
@@ -0,0 +1,25 @@
+version=$(head -n 1 ../project.version)
+source=$(echo $ref | cut -d / -f2-2)
+tagConversion=$(echo $ref | cut -d / -f3- | tr  '/' '-')
+
+if [[ "$source" == "heads" ]]
+then
+  if [[ "$tagConversion" == "main" ]]
+  then
+    tag="$version"
+  else
+    tag="$version-$tagConversion"
+  fi
+elif [[ "$source" == "tags" ]]
+then
+  tag="$version-$tagConversion"
+elif [[ "$source" == "pull" ]]
+then
+  pr=$(echo $ref |  cut -d / -f3-3)
+  tag="$version-rc$pr"
+else
+   tag="$version-unknown"
+fi
+
+echo "$tag"
+
